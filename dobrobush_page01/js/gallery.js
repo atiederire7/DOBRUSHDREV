ed97cd97e77dad735bf8f94dc06db130a45893e3
atiederire7 <atiederire1920@gmail.com>
2020-09-26 18:00:25 +0300

    update-26.09.2020.18:00

---
+++ dobrobush_page01/js/gallery.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    	arrows:false,
+    	dots:true,
+    	adaptiveHeight:true,
+    	slidesToShow:1,
+    	slidesToScroll: 1,
+    	speed: 500,
+    	easing: 'ease',
+    	infinite: false,
+    	// initialSlide:0;
+    	autoplay: true,
+    	autoplaySpeed: 1000,
+    	pauseOnFocus:true,
+    	pauseOnHover: true,
+		pauseOnDotsHover:true,
+    	draggable: true,
+    	swipe: true,
+    	// touchTreshold: 10,
+    	touchMove: true,
+    	// waitForAnimate:false,
+    	centerMode: false,
+    	variableWidth: false,
+    	// rows: 1,
+    	// slidesPerRow: 1,
+    	// vertical: false,
+		// verticalSwiping: false,
+
+    });
+});
